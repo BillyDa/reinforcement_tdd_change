7c5a7a7417dfb4ace90594b253cac29d040d2b45
Billy Davidson <billy.davids52@gmail.com>
2018-09-04 15:35:55 -0400

    complete

---
+++ changer.rb
@@ -0,0 +1,14 @@
+class Changer
+
+  def self.make_change(amount)
+    number = []
+    coins = [25, 10, 5, 1]
+      coins.each do |c|
+        while amount >= c
+          amount -= c
+          number << c
+        end
+      end
+    return number.sort.reverse
+  end
+end
